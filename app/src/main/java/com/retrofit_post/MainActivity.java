1467f40f42d82baaf3d70570faec461bca1e11cd
c1m0ham3dd0tjar <cimo.hamed.old@gmail.com>
2020-12-01 00:07:05 +0100

    add put patch example

--- app/src/main/java/com/retrofit_post/MainActivity.java
+++ app/src/main/java/com/retrofit_post/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -28,11 +29,62 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                callRetrofit();
+                patchRetrofiData();
+//                updateretrofiData();
+//                callRetrofit();
             }
         });
     }
+    private void updateretrofiData(){
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("https://httpbin.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
+        PostRequestAPI postRequestAPI=retrofit.create(PostRequestAPI.class);
+        PostModel postModel=new PostModel("post 55","post Changed Successfully");
+
+        Call<PostModel> callupdate=postRequestAPI.UdpateData(postModel);
+
+        callupdate.enqueue(new Callback<PostModel>() {
+            @Override
+            public void onResponse(Call<PostModel> call, Response<PostModel> response) {
+                txtv.setText(response.body().getJson().getData());
+                Toast.makeText(MainActivity.this,"Success "+response.code(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<PostModel> call, Throwable t) {
+
+            }
+        });
+
+    }
+    private void patchRetrofiData(){
+        Retrofit retrofit=new Retrofit.Builder()
+                .baseUrl("https://httpbin.org/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        PostRequestAPI postRequestAPI=retrofit.create(PostRequestAPI.class);
+        PostModel postModel=new PostModel("patch title","patched");
+
+        Call<PostModel> callupdate=postRequestAPI.PatchData(postModel);
+
+        callupdate.enqueue(new Callback<PostModel>() {
+            @Override
+            public void onResponse(Call<PostModel> call, Response<PostModel> response) {
+                txtv.setText(response.body().getJson().getData());
+                Toast.makeText(MainActivity.this,"Success "+response.code(),Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<PostModel> call, Throwable t) {
+
+            }
+        });
+
+    }
     private void callRetrofit() {
             String txt=edtxt.getText().toString();
         Retrofit retrofit=new Retrofit.Builder()
--- app/src/main/java/com/retrofit_post/PostRequestAPI.java
+++ app/src/main/java/com/retrofit_post/PostRequestAPI.java
@@ -2,10 +2,21 @@ package com.retrofit_post;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.PATCH;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface PostRequestAPI {
 
     @POST("post")
     Call<PostModel> PostDataIntoServer(@Body PostModel postModel);
+
+
+    @PUT("put")
+    Call<PostModel> UdpateData(@Body PostModel postModel);
+
+    @PATCH("patch")
+    Call<PostModel> PatchData(@Body PostModel postModel);
+
+
 }
